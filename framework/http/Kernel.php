5ebdd16c730c869d8ba3caaa082ddef6d0f03213
Kamil <mr.kaam@gmail.com>
2023-09-27 14:33:15 +0200

    Feat: create routes

--- framework/http/Kernel.php
+++ framework/http/Kernel.php
@@ -2,12 +2,46 @@
 
 namespace MvcPurePhp\Framework\Http;
 
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
     public function handle(Request $request) : Response
     {
-        $content = '<h1>Some other content :-) </h1>';
+        // Create a dispatcher
+        $dispatcher = simpleDispatcher(function (RouteCollector $r){
+            $r->addRoute('GET', '/', function(){
+                $content = "<h1>Jestem w routerze</h1>";
 
-        return new Response(content: $content);
+                return new Response($content);
+            });
+
+            $r->addRoute('GET', '/user/{id: \d+}', function($routeParams){
+
+                $content = <<<HTML
+                    <h1>Podstrona nr 2</h1>
+                    <p>Przekazano parametr {$routeParams['id']}</p>
+                HTML;;
+
+                return new Response($content);
+            });
+        });
+
+        // Dispatch a URI, to obtain the route info
+        $routeInfo = $dispatcher->dispatch(
+            httpMethod: $request->server['REQUEST_METHOD'],
+            uri: $request->server['REQUEST_URI']
+        );
+
+
+        // [$status, $handlers, $vars] = $routeInfo;  || simpliest notation
+        $status = $routeInfo[0];
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+
+        // Call the handler, provided by the route info, in order to create a Response
+
+        return $handler($vars);
     }
 }
