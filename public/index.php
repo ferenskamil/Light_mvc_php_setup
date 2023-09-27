a9102ef832c882d295b272e99c54c3122d8246e5
Kamil <mr.kaam@gmail.com>
2023-09-27 12:42:58 +0200

    Initial commit: basic architecture, autoload, composer, symfony/var-dumper

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+/** STEPS
+ *  1. request received
+ *  2. perform some logic
+ *  3. send response (string of content) (or JSON content) */
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+/** To delete in future */
+ $var = ['test1' , 'test2'];
+ dd($var);
+ echo "<h1>Temporary Main page</h1>";
+
+
