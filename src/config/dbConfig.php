870710dd65f2486df73deda4e15d206bcfe47bdf
Kamil <mr.kaam@gmail.com>
2023-09-27 20:29:21 +0200

    Feat: Database Config (DTO)

--- src/config/dbConfig.php
+++ src/config/dbConfig.php
@@ -1,4 +1,29 @@
 <?php
 
-/** DTO (zmienne prywatne i gettery) */
-/*** Configi do bazy danych  */
+namespace App\Config;
+class DbConfig
+{
+    private string $hostname = 'localhost';
+    private string $dbName = 'exampleDatabase';
+    private string $username = 'root';
+    private string $password = '';
+
+    public function getHostname() : string
+    {
+        return $this->hostname;
+    }
+    public function getDbName() : string
+    {
+        return $this->dbName;
+    }
+
+    public function getPassword() : string
+    {
+        return $this->password;
+    }
+
+    public function getUsername() : string
+    {
+        return $this->username;
+    }
+}
